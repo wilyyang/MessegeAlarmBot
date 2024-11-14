8cfd0f862c7bc6e7b3e2437257a7a9be9d6f0a8f
wilyyang <ehdrnr1178@gmail.com>
2024-11-14 22:08:31 +0900

    코드 수정

--- app/src/main/kotlin/com/messege/alarmbot/contents/CommonContent.kt
+++ app/src/main/kotlin/com/messege/alarmbot/contents/CommonContent.kt
@@ -5,6 +5,7 @@ import com.messege.alarmbot.core.common.helpKeyword
 import com.messege.alarmbot.core.common.hostKeyword
 import com.messege.alarmbot.core.common.ChatRoomKey
 import com.messege.alarmbot.core.common.CommonText
+import com.messege.alarmbot.core.common.TARGET_KEY
 import com.messege.alarmbot.data.database.user.model.UserData
 import kotlinx.coroutines.channels.Channel
 
@@ -16,19 +17,21 @@ class CommonContent(
     override val contentsName: String = "기본"
 
     override suspend fun request(postTime : Long, chatRoomKey: ChatRoomKey, user : Person, text : String) {
-        when(text){
-            hostKeyword + helpKeyword -> {
-                commandChannel.send(MainChatTextResponse(text = CommonText.HELP))
-            }
-            else -> {
-                val currentName = "${user.name}"
-                val currentKey = "${user.key}"
-                val userNameList = getUserNameList(currentKey)
-                if(userNameList.isEmpty()){
-                    insertUser(UserData(updateTime = postTime, userKey = currentKey, name = currentName))
-                }else if(userNameList.first() != currentName){
-                    insertUser(UserData(updateTime = postTime, userKey = currentKey, name = currentName))
-                    commandChannel.send(MainChatTextResponse(text = CommonText.alreadyUser(name = currentName, alreadyUsers = userNameList)))
+        if(chatRoomKey == TARGET_KEY){
+            when(text){
+                hostKeyword + helpKeyword -> {
+                    commandChannel.send(MainChatTextResponse(text = CommonText.HELP))
+                }
+                else -> {
+                    val currentName = "${user.name}"
+                    val currentKey = "${user.key}"
+                    val userNameList = getUserNameList(currentKey)
+                    if(userNameList.isEmpty()){
+                        insertUser(UserData(updateTime = postTime, userKey = currentKey, name = currentName))
+                    }else if(userNameList.first() != currentName){
+                        insertUser(UserData(updateTime = postTime, userKey = currentKey, name = currentName))
+                        commandChannel.send(MainChatTextResponse(text = CommonText.alreadyUser(name = currentName, alreadyUsers = userNameList)))
+                    }
                 }
             }
         }
--- app/src/main/kotlin/com/messege/alarmbot/contents/MafiaGameContent.kt
+++ app/src/main/kotlin/com/messege/alarmbot/contents/MafiaGameContent.kt
@@ -3,6 +3,7 @@ package com.messege.alarmbot.contents
 import android.app.Person
 import com.messege.alarmbot.core.common.ChatRoomKey
 import com.messege.alarmbot.core.common.CommonText
+import com.messege.alarmbot.core.common.MafiaText
 import com.messege.alarmbot.core.common.helpKeyword
 import kotlinx.coroutines.channels.Channel
 
@@ -14,7 +15,7 @@ class MafiaGameContent(
     override suspend fun request(postTime : Long, chatRoomKey: ChatRoomKey, user : Person, text : String) {
         when(text){
             contentsName + helpKeyword -> {
-                commandChannel.send(MainChatTextResponse(text = CommonText.HELP))
+                commandChannel.send(MainChatTextResponse(text = MafiaText.HELP))
             }
             else -> {
             }
