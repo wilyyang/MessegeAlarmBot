dd376fc0888790c05b865201748e4e04095bb08c
wily.yang <ehdrnr1178@gmail.com>
2025-03-13 16:28:28 +0900

    명령어 임시저장

--- app/src/main/kotlin/com/messege/alarmbot/contents/party/PartyContent.kt
+++ app/src/main/kotlin/com/messege/alarmbot/contents/party/PartyContent.kt
@@ -8,8 +8,11 @@ import com.messege.alarmbot.data.database.member.dao.MemberDatabaseDao
 import com.messege.alarmbot.processor.model.Command
 import com.messege.alarmbot.processor.model.Group1RoomTextResponse
 import com.messege.alarmbot.processor.model.Message
+import com.messege.alarmbot.processor.usecase.PartyCreateResult
+import com.messege.alarmbot.processor.usecase.PartyDissolveResult
 import com.messege.alarmbot.processor.usecase.PartyMemberEvent
 import com.messege.alarmbot.processor.usecase.UseCaseParty
+import com.messege.alarmbot.util.format.toTimeFormatDate
 import com.messege.alarmbot.util.log.Logger
 import kotlinx.coroutines.channels.Channel
 
@@ -33,11 +36,33 @@ class PartyContent (
                         if (args.size > 1) {
                             val partyName = args[1]
                             val result = useCaseParty.createParty(member, partyName)
+                            val response = when(result){
+                                is PartyCreateResult.CreateSuccess -> {
+                                    "창당에 성공했습니다!! 정당 이름은 $partyName 입니다. (help : .? 정당)"
+                                }
+                                is PartyCreateResult.AlreadyPartyMember -> {
+                                    "이미 정당에 가입해 있습니다. 탈당 후 창당하세요."
+                                }
+
+                                is PartyCreateResult.AlreadyPartyNameExist -> {
+                                    "기존 정당 이름에 $partyName 이 있습니다. 다른 이름을 사용하세요."
+                                }
+                            }
+                            commandChannel.send(Group1RoomTextResponse(response))
 
                         } else responseTextFormatInvalid()
                     }
                     message.text == ".${PartyKeyword.PARTY_DISSOLVE}" -> {
                         val result = useCaseParty.dissolveParty(member)
+                        val response = when(result){
+                            is PartyDissolveResult.DissolveSuccess -> {
+                                "${result.party.name} 정당을 해산했습니다. 모든 당원의 당적이 초기화되었습니다."
+                            }
+                            is PartyDissolveResult.DissolveFail -> {
+                                "정당을 해산할 권리가 없습니다."
+                            }
+                        }
+                        commandChannel.send(Group1RoomTextResponse(response))
 
                     }
                     message.text.startsWith(".${PartyKeyword.PARTY_INFO} ") -> {
@@ -46,6 +71,24 @@ class PartyContent (
                             val partyName = args[1]
                             val result = useCaseParty.getPartyInfo(partyName)
 
+
+                            val response = if(result != null){
+                                val members = result.partyMembers.joinToString(", "){
+                                    it.latestName
+                                }
+
+                                "[${result.partyInfo.name} 정보]\n" +
+                                        "- 창당일 : ${result.partyInfo.foundingTime.toTimeFormatDate()}\n" +
+                                        "- 당대표 : ${result.partyLeader.latestName}\n" +
+                                        "- 당원수 : ${result.partyInfo.memberCount}명\n" +
+                                        "- 당소개 : ${result.partyInfo.description}\n" +
+                                        "- 당원 : $members"
+
+                            }else{
+                                "정당 정보가 없습니다."
+                            }
+
+                            commandChannel.send(Group1RoomTextResponse(response))
                         } else responseTextFormatInvalid()
                     }
                     message.text == ".${PartyKeyword.PARTY_RANKING}" -> {
@@ -108,10 +151,10 @@ class PartyContent (
                             val result = useCaseParty.delegateLeader(member, mentionId)
                             val response = when(result){
                                 is PartyMemberEvent.Success -> {
-                                    "${PartyKeyword.PARTY_MEM_DELEGATE} 했습니다."
+                                    "[${PartyKeyword.PARTY_MEM_DELEGATE}] 했습니다."
                                 }
                                 else -> {
-                                    "${PartyKeyword.PARTY_MEM_DELEGATE} 실패 했습니다."
+                                    "[${PartyKeyword.PARTY_MEM_DELEGATE}] 실패 했습니다."
                                 }
                             }
                             commandChannel.send(Group1RoomTextResponse(response))
@@ -124,10 +167,10 @@ class PartyContent (
                             val result = useCaseParty.requestToJoinParty(member, partyName)
                             val response = when(result){
                                 is PartyMemberEvent.Success -> {
-                                    "${PartyKeyword.PARTY_MEM_JOIN} 했습니다."
+                                    "[${PartyKeyword.PARTY_MEM_JOIN}] 했습니다."
                                 }
                                 else -> {
-                                    "${PartyKeyword.PARTY_MEM_JOIN} 실패 했습니다."
+                                    "[${PartyKeyword.PARTY_MEM_JOIN}] 실패 했습니다."
                                 }
                             }
                             commandChannel.send(Group1RoomTextResponse(response))
@@ -137,10 +180,10 @@ class PartyContent (
                         val result = useCaseParty.cancelJoinRequest(member)
                         val response = when(result){
                             is PartyMemberEvent.Success -> {
-                                "${PartyKeyword.PARTY_MEM_CANCEL} 했습니다."
+                                "[${PartyKeyword.PARTY_MEM_CANCEL}] 했습니다."
                             }
                             else -> {
-                                "${PartyKeyword.PARTY_MEM_CANCEL} 실패 했습니다."
+                                "[${PartyKeyword.PARTY_MEM_CANCEL}] 실패 했습니다."
                             }
                         }
                         commandChannel.send(Group1RoomTextResponse(response))
@@ -153,10 +196,10 @@ class PartyContent (
                             val result = useCaseParty.approveMemberRequest(member, mentionId)
                             val response = when(result){
                                 is PartyMemberEvent.Success -> {
-                                    "${PartyKeyword.PARTY_MEM_APPROVE} 했습니다."
+                                    "[${PartyKeyword.PARTY_MEM_APPROVE}] 했습니다."
                                 }
                                 else -> {
-                                    "${PartyKeyword.PARTY_MEM_APPROVE} 실패 했습니다."
+                                    "[${PartyKeyword.PARTY_MEM_APPROVE}] 실패 했습니다."
                                 }
                             }
                             commandChannel.send(Group1RoomTextResponse(response))
@@ -170,10 +213,10 @@ class PartyContent (
                             val result = useCaseParty.rejectMemberRequest(member, mentionId)
                             val response = when(result){
                                 is PartyMemberEvent.Success -> {
-                                    "${PartyKeyword.PARTY_MEM_REJECT} 했습니다."
+                                    "[${PartyKeyword.PARTY_MEM_REJECT}] 했습니다."
                                 }
                                 else -> {
-                                    "${PartyKeyword.PARTY_MEM_REJECT} 실패 했습니다."
+                                    "[${PartyKeyword.PARTY_MEM_REJECT}] 실패 했습니다."
                                 }
                             }
                             commandChannel.send(Group1RoomTextResponse(response))
@@ -183,10 +226,10 @@ class PartyContent (
                         val result = useCaseParty.leaveParty(member)
                         val response = when(result){
                             is PartyMemberEvent.Success -> {
-                                "${PartyKeyword.PARTY_MEM_LEAVE} 했습니다."
+                                "[${PartyKeyword.PARTY_MEM_LEAVE}] 했습니다."
                             }
                             else -> {
-                                "${PartyKeyword.PARTY_MEM_LEAVE} 실패 했습니다."
+                                "[${PartyKeyword.PARTY_MEM_LEAVE}] 실패 했습니다."
                             }
                         }
                         commandChannel.send(Group1RoomTextResponse(response))
@@ -200,10 +243,10 @@ class PartyContent (
                             val result = useCaseParty.expelMember(member, mentionId)
                             val response = when(result){
                                 is PartyMemberEvent.Success -> {
-                                    "${PartyKeyword.PARTY_MEM_EXPULSION} 했습니다."
+                                    "[${PartyKeyword.PARTY_MEM_EXPULSION}] 했습니다."
                                 }
                                 else -> {
-                                    "${PartyKeyword.PARTY_MEM_EXPULSION} 실패 했습니다."
+                                    "[${PartyKeyword.PARTY_MEM_EXPULSION}] 실패 했습니다."
                                 }
                             }
                             commandChannel.send(Group1RoomTextResponse(response))
