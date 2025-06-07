9d1ff54dc9b106d94ec8b7d9a20fb6a2c595c48b
wilyyang <ehdrnr1178@gmail.com>
2025-06-07 15:43:51 +0900

    봇 요약 기능 추가

--- app/src/main/kotlin/com/messege/alarmbot/contents/bot/BotContent.kt
+++ app/src/main/kotlin/com/messege/alarmbot/contents/bot/BotContent.kt
@@ -3,7 +3,12 @@ package com.messege.alarmbot.contents.bot
 import com.google.ai.client.generativeai.type.Content
 import com.google.ai.client.generativeai.type.content
 import com.messege.alarmbot.contents.BaseContent
+import com.messege.alarmbot.contents.bot.Constants.BOT_ID
+import com.messege.alarmbot.contents.bot.Constants.BOT_START_PREFIX
+import com.messege.alarmbot.contents.bot.Constants.CHAT_BOT_ID
+import com.messege.alarmbot.contents.bot.Constants.MESSAGE_TEXT_LIMIT
 import com.messege.alarmbot.contents.bot.Constants.REQUEST_POINT
+import com.messege.alarmbot.contents.bot.Constants.REQUEST_SUMMARY_POINT
 import com.messege.alarmbot.core.common.ChatRoomType
 import com.messege.alarmbot.data.database.member.dao.MemberDatabaseDao
 import com.messege.alarmbot.processor.model.Command
@@ -12,31 +17,60 @@ import com.messege.alarmbot.processor.model.Message
 import com.messege.alarmbot.util.log.Logger
 import kotlinx.coroutines.channels.Channel
 
+data class BotMessage(val userId: Long, val message: String)
+
 class BotContent(
     override val commandChannel : Channel<Command>,
     private val memberDatabaseDao : MemberDatabaseDao
 ) : BaseContent {
     override val contentsName : String = "봇"
     private val historyCache : MutableList<Content> = mutableListOf()
+    private val messageCache : MutableList<BotMessage> = mutableListOf()
 
     override suspend fun request(message : Message) {
-        if (message.type == ChatRoomType.GroupRoom1 && message is Message.Talk &&
-            message.text.length in (Constants.BOT_START_PREFIX.length + 1) .. Constants.REQUEST_TEXT_LIMIT &&
-            message.text.startsWith(Constants.BOT_START_PREFIX)
-        ) {
-            val user = memberDatabaseDao.getMember(message.userId).getOrNull(0)
-            if (user != null) {
-                if (user.giftPoints >= REQUEST_POINT) {
-                    val question = message.text.substring(Constants.BOT_START_PREFIX.length).trim()
-                    if (question.isBlank()) return
+        if (message.type == ChatRoomType.GroupRoom1 && message is Message.Talk && message.userId != BOT_ID && message.userId != CHAT_BOT_ID) {
 
-                    val response = handleBotQuestion("#${user.latestName}# $question")
-                    if (response != null) {
-                        memberDatabaseDao.updateMemberGiftPoints(user.userId, user.giftPoints - REQUEST_POINT)
-                        commandChannel.send(Group1RoomTextResponse(response))
+            if(!message.text.startsWith(BOT_START_PREFIX)){
+                messageCache += BotMessage(message.userId, message.text)
+                while (messageCache.size > Constants.MAX_MESSAGE_SIZE) {
+                    messageCache.removeAt(0)
+                }
+            }
+
+            if (message.text.startsWith(Constants.BOT_SUMMARY_START_PREFIX) &&
+                message.text.length <= MESSAGE_TEXT_LIMIT
+            ) {
+
+                val user = memberDatabaseDao.getMember(message.userId).getOrNull(0)
+                if (user != null) {
+                    if (user.giftPoints >= REQUEST_SUMMARY_POINT) {
+                        val response = handleBotSummaryQuestion("#${user.latestName}# ${message.text}")
+                        if (response != null) {
+                            memberDatabaseDao.updateMemberGiftPoints(user.userId, user.giftPoints - REQUEST_SUMMARY_POINT)
+                            commandChannel.send(Group1RoomTextResponse("[요약]\n$response"))
+                        }
+                    } else {
+                        commandChannel.send(Group1RoomTextResponse("요약하려면 $REQUEST_SUMMARY_POINT 포인트가 필요합니다. (현재 포인트 : ${user.giftPoints})"))
                     }
-                } else {
-                    commandChannel.send(Group1RoomTextResponse("질문하려면 $REQUEST_POINT 포인트가 필요합니다. (현재 포인트 : ${user.giftPoints})"))
+                }
+            }else if (message.text.length in (Constants.BOT_START_PREFIX.length + 1) .. Constants.REQUEST_TEXT_LIMIT &&
+                message.text.startsWith(Constants.BOT_START_PREFIX)
+            ) {
+
+                val user = memberDatabaseDao.getMember(message.userId).getOrNull(0)
+                if (user != null) {
+                    if (user.giftPoints >= REQUEST_POINT) {
+                        val question = message.text.substring(Constants.BOT_START_PREFIX.length).trim()
+                        if (question.isBlank()) return
+
+                        val response = handleBotQuestion("#${user.latestName}# $question")
+                        if (response != null) {
+                            memberDatabaseDao.updateMemberGiftPoints(user.userId, user.giftPoints - REQUEST_POINT)
+                            commandChannel.send(Group1RoomTextResponse(response))
+                        }
+                    } else {
+                        commandChannel.send(Group1RoomTextResponse("질문하려면 $REQUEST_POINT 포인트가 필요합니다. (현재 포인트 : ${user.giftPoints})"))
+                    }
                 }
             }
         }
@@ -60,6 +94,33 @@ class BotContent(
                 }
                 return replyText
             }
+        } catch (e : Exception) {
+            Logger.e("[ai.error] ${e.message}")
+        }
+        return null
+    }
+
+    private suspend fun handleBotSummaryQuestion(question : String) : String? {
+        try {
+            val messageContents = messageCache.mapNotNull { botMessage ->
+                val user = memberDatabaseDao.getMember(botMessage.userId).getOrNull(0)
+
+                if (user != null) {
+                    val message = botMessage.message
+                    val messageCut = if (message.length > MESSAGE_TEXT_LIMIT) message.substring(0, 1000) else message
+                    content { role = ROLE_USER; text("#${user.latestName}# $messageCut") }
+                } else {
+                    null
+                }
+            }
+
+            val chat = generativeModel.startChat(
+                history = listOf(summaryPrompt) + messageContents
+            )
+
+            val response = chat.sendMessage(question)
+            val replyText = response.text?.trim().orEmpty()
+            return replyText
         } catch (e : Exception) {
             Logger.e("[ai.error] ${e.message}")
         }
--- app/src/main/kotlin/com/messege/alarmbot/contents/bot/BotObject.kt
+++ app/src/main/kotlin/com/messege/alarmbot/contents/bot/BotObject.kt
@@ -6,9 +6,15 @@ import com.google.ai.client.generativeai.type.content
 object Constants {
     const val apiKey = "AIzaSyCWhHWdZdXP3DaeUJioTEvn_p9K1K3ztvE"
     const val BOT_START_PREFIX = "또봇"
+    const val BOT_SUMMARY_START_PREFIX = "또봇 요약"
+    const val BOT_ID = 421448547L
+    const val CHAT_BOT_ID = 8201629100799150694L
     const val REQUEST_TEXT_LIMIT = 1000
+    const val MESSAGE_TEXT_LIMIT = 1000
     const val MAX_HISTORY_SIZE = 15
+    const val MAX_MESSAGE_SIZE = 40
     const val REQUEST_POINT = 2
+    const val REQUEST_SUMMARY_POINT = 0
 }
 
 // Gemini 모델
@@ -32,6 +38,19 @@ val systemPrompt = content {
 질문자의 이름은 항상 질문글 앞에 #이름# 형식으로 명시된다.
 너는 히스토리를 통해 각 질문글을 질문자별로 구분해야만 한다.
 질문자가 자신의 이름을 물어보면 형식에 명시한 이름을 말해주고, 그렇지 않으면 언급하지마. (이 때 당연히 # 문자를 제외한 이름이다)
+""".trimIndent()
+    )
+}
+
+val summaryPrompt = content {
+    role = ROLE_USER
+    text(
+        """
+너는 히스토리를 통해 각 메시지를 유저 이름 별로 구분해야만 한다.
+유저 이름은 항상 질문글 앞에 #이름# 형식으로 명시된다.
+'또봇 요약'으로 시작하는 질문을 받으면, 히스토리에 저장된 메시지를 유저 이름 별로 요약하고 정리해야 한다.
+이 질문의 응답은 히스토리의 각 유저 간 대화가 중요하다.
+유저 이름 좌우에 붙는 #은 형식일 뿐이야. 그러므로 너의 응답에 #문자가 들어가선 안된다. 어떤 경우에도 #문자를 제거해줘.
 """.trimIndent()
     )
 }
